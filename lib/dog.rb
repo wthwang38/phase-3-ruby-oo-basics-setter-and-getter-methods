d0ae28ebcc708c4dc7be9c45072c884ed791cbdb
William Hwang <willhwang90@gmail.com>
2022-06-09 12:34:53 -0400

    commit tree ae5a9b4c1187eac85d364ca4e1b09b137d029d14

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,3 +1,19 @@
 class Dog
 
+    def name
+        @name
+    end
+
+    def name=(value)
+        @name = value
+    end
+
+    def breed
+        @breed
+    end
+    def breed=(value)
+        @breed = value
+    end
+
+    
 end
